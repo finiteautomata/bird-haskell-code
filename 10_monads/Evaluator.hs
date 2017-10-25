1483d7ac4225cb7866fcdf558f05071655b9f9a4
Juan Manuel Perez <jmperez.85@gmail.com>
2017-10-25 08:43:59 -0300

    Monadic evaluator.

--- 10_monads/Evaluator.hs
+++ 10_monads/Evaluator.hs
@@ -1,15 +1,10 @@
-import Monad
+-- import Monad
 import Exception
 
 data Term = Con Int | Div Term Term
 
-eval :: Term -> Exc Int
-eval (Con x) = Return x
-eval (Div t1 t2) = h (eval t1)
-  where h (Raise e) = Raise e
-        h (Return x) = h' (eval t2)
-          where
-            h'(Raise e) = Raise e
-            h'(Return y) = if y == 0
-                             then Raise "division by zero"
-                             else Return (div x y)
+eval :: Monad m => Term -> m Int
+eval (Con x) = return x
+eval (Div t1 t2) = do x <- eval t1
+                      y <- eval t2
+                      return (div x y)
